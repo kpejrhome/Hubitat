fea847b6632d90d41f1cacca4858faaa3200ec1e
kpejrhome <67492283+kpejrhome@users.noreply.github.com>
2021-02-18 20:04:31 -0500

    Updated url

--- kpejr-color-bulb-notification-child.groovy
+++ kpejr-color-bulb-notification-child.groovy
@@ -26,7 +26,7 @@ definition(
     description: "Changes color bulb when contact is open",
     category: "Convenience",
     parent: "kpejr:Color Bulb Notification",
-    importUrl: "https://github.com/kpejrhome/Hubitat/raw/master/kpejr-color-bulb-notification-child.groovy",
+    importUrl: "https://raw.githubusercontent.com/kpejrhome/Hubitat/master/kpejr-color-bulb-notification-child.groovy",
     
     iconUrl: "",
     iconX2Url: "",
