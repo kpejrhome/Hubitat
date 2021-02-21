cb359646668618177393fe51bbc598179f15969c
kpejrhome <67492283+kpejrhome@users.noreply.github.com>
2021-02-20 22:42:26 -0500

    Added debug logging option.

--- kpejr-group-switch-parent.groovy
+++ kpejr-group-switch-parent.groovy
@@ -6,8 +6,9 @@
 *
 *  Documentation:  Use a virtual or real switch to group other switches
 *
-*  Changelog: V1.0
- 
+*  Changelog: v1.0
+*  V1.0.1 - Added debug logging option
+*
 *
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 *  in compliance with the License. You may obtain a copy of the License at:
@@ -25,7 +26,7 @@ definition(
     author: "Kevin Earley",
     description: "Use a virtual or real switch to group other switches",
     category: "Convenience",
-    importUrl: "https://raw.githubusercontent.com/kpejrhome/Hubitat/master/kpejr-group-switch-parent.groovy",
+    importUrl: "https://raw.githubusercontent.com/kpejrhome/Hubitat/master/kpejr-group-switch.groovy",
 	    
     iconUrl: "",
     iconX2Url: "",
@@ -37,32 +38,42 @@ preferences {
      page name: "mainPage", title: "", install: true, uninstall: true
 } 
 
-def installed() {
-    log.info "Installed with settings: ${settings}"
-    initialize()
-}
-
-
-def updated() {
-    log.info "Updated with settings: ${settings}"
-    unsubscribe()
-    initialize()
-}
-
-def initialize() {
-  
-    childApps.each { child ->
-        log.info "Child app: ${child.label}"
-    }
-}
-
 def mainPage() {
     dynamicPage(name: "mainPage") {
                 
         section() {
             app(name: "newGroup", appName: "Group Switch - Child", namespace: "kpejr", title: "<b>Add a Group Switch app</b>", multiple: true)
+            input name:	"enableLogging", type: "bool", title: "Enable Debug Logging?", defaultValue: true, required: true
         }		
 	}
 }
 
- 
+def installed() {
+    logDebug("Installed application")
+    
+    initialize()
+}
+
+
+def updated() {
+    logDebug("Updated application")
+    
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    logDebug("Updated with settings: ${settings}")
+    
+    childApps.each { child ->
+        logDebug("Child app: ${child.label}")
+    }
+}
+
+def logDebug(msg)
+{
+    if(enableLogging)
+    {
+        log.debug "${msg}"
+    }
+}
